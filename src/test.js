d4fefcca737b41fc62b900c362dec9e2f3120515
milind-nair <nairmilind3@gmail.com>
2023-11-03 21:23:25 +0530

    converted into ESM module call

--- src/test.js
+++ src/test.js
@@ -1,6 +1,5 @@
 // Example usage:
-translateSpringCronExpression = require("./script.js");
-// import translateSpringCronExpression from "./script.js";
+import translateSpringCronExpression from "./script.js";
 const cronExpression = "0 0 0 ? * MON#1";
 const humanReadable = translateSpringCronExpression(cronExpression);
 console.log(humanReadable);
