c7dd10f097ea5c4214f32695d07883ddd64671fb
milind-nair <nairmilind3@gmail.com>
2023-11-03 21:23:04 +0530

    extracted constants

---
+++ src/constants.js
@@ -0,0 +1,32 @@
+export const cronFieldNames = [
+  "second",
+  "minute",
+  "hour",
+  "day of the month",
+  "month",
+  "day of the week",
+];
+export const dayMap = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+export const monthMap = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
