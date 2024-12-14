a6973a8a8c5128f8beecc3a8780e88fa0afad6f1
milind-nair <nairmilind3@gmail.com>
2024-12-14 23:36:46 +0530

    sample run script

--- src/run.js
+++ src/run.js
@@ -1,5 +1,9 @@
 // Example usage:
 import translateSpringCronExpression from "./script.js";
-const cronExpression = "0 0 0 ? * MON#1";
+const cronExpression = "1 0 0 ? * MON#1";
 const humanReadable = translateSpringCronExpression(cronExpression);
 console.log(humanReadable);
+
+// switch toggle -verbose  =True 
+//  First 
+// 1st Monday of the month at midnight.
